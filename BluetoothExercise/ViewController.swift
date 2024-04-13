715575d9ba38d232d059a6e74edd5c2beb817280
Logan <logan.hsiao.jk@gmail.com>
2024-04-13 11:24:44 +0800

    Initial Commit

---
+++ BluetoothExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothExercise
+//
+//  Created by LoganMacMini on 2024/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
